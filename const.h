d114392838dda3e46e57a8c75178c3ad91aedc15
davidwangd <davidwangnoi@163.com>
2017-08-30 02:21:09 +0800

    修改

--- const.h
+++ const.h
@@ -5,9 +5,10 @@
 const QString btnPushedStyle = "background-color:rgb(255,227,132)";
 const QString btnHighlightSyle = "background-color:rgb(135,206,255)";
 const QString btnHighlightSecondStyle = "background-color:rgb(176,224,230)";
-const QString btnNormalStyle = "background-color:rgb(255,255,255)";
-const QFont numberNormalFont = QFont("Courier New", 20, 35);
+const QString btnNormalStyle = "";
+const QFont numberNormalFont = QFont("Courier New", 20, 15);
 const QFont numberNoteFont = QFont("Courier New", 8, 5);
-const QFont numberQustionFont = QFont("Courier", 20, 15, 1);
+const QFont numberQustionFont = QFont("Algerian", 20, 15, 1);
+
 const int SIZE = 9;
 #endif // CONST_H
